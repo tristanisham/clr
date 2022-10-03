f0e5c5d5d88d6cc94cdde36653cec825577d3d15
Tristan Isham <tristan@isham.co>
2022-10-03 19:34:49 -0400

    added docs

--- lib.go
+++ lib.go
@@ -1,3 +1,6 @@
+// Package clr is a small package for coloring terminal text output with a pleasent to use API.
+// Many Go terminal coloring packages are highly functional, which is nice for legibility but a bit of a pain
+// when you're just trying to color a sentence or two. 
 package clr
 
 // Black takes your input and wraps it in ANSI color code 30
