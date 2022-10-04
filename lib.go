00ee60046d85f02cea966e517a339a380685dc1b
Tristan Isham <tristan@isham.co>
2022-10-03 20:16:24 -0400

    updated grammar

--- lib.go
+++ lib.go
@@ -1,4 +1,4 @@
-// Package clr is a small package for coloring terminal text output with a pleasent to use API.
+// Package clr is a small package for coloring terminal text output with a pleasent API.
 // Many Go terminal coloring packages are highly functional, which is nice for legibility but a bit of a pain
 // when you're just trying to color a sentence or two. 
 package clr
